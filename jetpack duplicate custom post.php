1d413afa6ca9ac6dfa5e9111ce2c0d14ca56b2d2
Priscila S Cunha <priscilacunha@msn.com>
2020-07-10 01:18:03 -0300

    Update jetpack duplicate custom post.php

--- jetpack duplicate custom post.php
+++ jetpack duplicate custom post.php
@@ -2,8 +2,8 @@
 
 /* Makes JetPack add duplicate option for custom posts - functions.php */
 /* Faz JetPack adicionar opção de duplicar para posts personalizados - functions.php */
+add_filter( 'jetpack_copy_post_post_types', 'jetpack_add_copy_support' );
 function jetpack_add_copy_support( $post_types ) {
     $post_types[] = 'portfolio'; // add post type name here
     return $post_types;
 }
-add_filter( 'jetpack_copy_post_post_types', 'jetpack_add_copy_support' );
