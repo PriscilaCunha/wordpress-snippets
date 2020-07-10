ab4550df70a61f7de575e539f6a4c9ccb792775b
Priscila S Cunha <priscilacunha@msn.com>
2020-07-10 01:20:34 -0300

    Create disable image resizing.php

---
+++ disable image resizing.php
@@ -0,0 +1,5 @@
+<?php
+
+/* Makes JetPack add duplicate option for custom posts - functions.php */
+/* Desabilita compactação de imagens automática do WordPress - functions.php */
+add_filter( 'big_image_size_threshold', '__return_false' );
