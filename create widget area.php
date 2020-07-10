b1f236223627be533b3e8b58a8f94eac5b0d8e98
Priscila S Cunha <priscilacunha@msn.com>
2020-07-09 23:32:28 -0300

    Update create widget area.php

--- create widget area.php
+++ create widget area.php
@@ -1,24 +1,27 @@
 <?php 
-/* Register sidebars and widgetized areas - functions.php */
+/* Register sidebars and widget areas - functions.php */
 /* Registrar barra lateral e área de widgets - functions.php */
 function custom_widgets_init() {
 
-    register_sidebar( array(
-        'name'          => 'Widget area name',
-        'description'   => __( 'Widget area description.', 'theme_text_domain' ),
-        'id'            => 'widget_id',
-        'before_widget' => '<div>',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h2 class="widget-title">',
-        'after_title'   => '</h2>',
-    ) );
+	register_sidebar( array(
+		'name'          => 'Widget area name',
+		'description'   => __( 'Widget area description.', 'theme_text_domain' ),
+		'id'            => 'widget_id',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+	
+	// Add others widget areas
+	// Adicionar outras áreas de widgets
 
 }
 add_action( 'widgets_init', custom_widgets_init );
 
 
-/* Display sidebars and widgetized areas - header.php, footer.php */
+/* Display sidebars and widget areas - header.php, footer.php */
 /* Exibir barra lateral e área de widgets - header.php, footer.php */
 if ( is_active_sidebar( 'widget_id' ) ) :
-    echo '<div id="widget-id" class="widget-area">'.dynamic_sidebar( 'widget_id' ).'</div><!-- #widget-id -->';
+	echo '<div id="widget-id" class="widget-area">'.dynamic_sidebar( 'widget_id' ).'</div><!-- #widget-id -->';
 endif;
