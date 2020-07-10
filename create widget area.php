2c57aa8173256641b98fe7f49f97480bba23ec9e
Priscila S Cunha <priscilacunha@msn.com>
2020-07-09 23:25:31 -0300

    Create create widget area.php

---
+++ create widget area.php
@@ -0,0 +1,26 @@
+<?php 
+
+/* Register sidebars and widgetized areas - functions.php */
+/* Registrar barra lateral e área de widgets - functions.php */
+function custom_widgets_init() {
+
+    register_sidebar( array(
+        'name'          => 'Widget area name',
+        'description'   => __( 'Widget area description.', 'theme_text_domain' ),
+        'id'            => 'widget_id',
+        'before_widget' => '<div>',
+        'after_widget'  => '</div>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
+
+}
+add_action( 'widgets_init', custom_widgets_init );
+
+?>
+
+/* Display sidebars and widgetized areas - header.php, footer.php */
+/* Exibir barra lateral e área de widgets - header.php, footer.php */
+<?php if ( is_active_sidebar( 'widget_id' ) ) : ?>
+    <div id="widget-id" class="widget-area"><?php dynamic_sidebar( 'widget_id' ); ?></div><!-- #widget-id -->
+<?php endif; ?>
